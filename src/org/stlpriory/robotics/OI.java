a9c36cbc9231bb8642ae6f91379699dfa89e253e
ajdEnder <ajd.ender@gmail.com>
2016-01-15 18:31:05 -0600

    Testing to see if I can figure out how to committ from Eclipse.  Still can't spell committ.  Sigh. That's why I teach math.

--- src/org/stlpriory/robotics/OI.java
+++ src/org/stlpriory/robotics/OI.java
@@ -19,6 +19,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * This class is the glue that binds the controls on the physical operator interface to the commands and command groups that allow control
  * of the robot.
  */
+
+// Testing for commit...
+
 public class OI {
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
