7fad7fbb3eb069dde88ac504d24b8622d71b664d
Will Koster <ddsnowboard@gmail.com>
2016-02-15 15:51:59 -0600

    Try to fix problem again

--- src/org/stlpriory/robotics/commands/HolderToBottom.java
+++ src/org/stlpriory/robotics/commands/HolderToBottom.java
@@ -7,9 +7,7 @@ import org.stlpriory.robotics.subsystems.BallHolderSubsystem.Direction;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class HolderToBottom extends Command {
-    public static final double MAX_ANGLE = BallHolderSubsystem.POTENTIOMETER_SCALE_FACTOR;
-    public static final double MIN_ANGLE = BallHolderSubsystem.POTENTIOMETER_OFFSET;
-    public static final double TOLERANCE = (1.0 / 15.0) * BallHolderSubsystem.POTENTIOMETER_SCALE_FACTOR;
+	
 
     public HolderToBottom() {
         requires(Robot.ballHolder);
@@ -18,36 +16,25 @@ public class HolderToBottom extends Command {
     @Override
     protected void initialize() {
         // TODO Auto-generated method stub
-
+//new BallHolderDown();
     }
 
     @Override
     protected void execute() {
-        // TODO Auto-generated method stub
-        Robot.ballHolder.set(Direction.DOWN, 1);
     }
 
     @Override
     protected boolean isFinished() {
-        System.out.printf("The limit is %f%n", Math.abs((MIN_ANGLE - TOLERANCE)));
-        if (Robot.ballHolder.getAngle() < Math.abs((MIN_ANGLE - TOLERANCE))) {
-            return true;
-        } else {
-            return false;
-        }
+    	return true;
     }
 
     @Override
     protected void end() {
-        // TODO Auto-generated method stub
-        Robot.ballHolder.set(0);
 
     }
 
     @Override
     protected void interrupted() {
-        // TODO Auto-generated method stub
-        Robot.ballHolder.set(0);
 
     }
 
