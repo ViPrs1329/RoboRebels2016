0beb2dc0b1ab1badca10196e673b18a849a6cf7f
Will Koster <ddsnowboard@gmail.com>
2016-03-04 18:40:39 -0600

    Remove code of unknown origin

--- src/org/stlpriory/robotics/commands/Vibrator.java
+++ src/org/stlpriory/robotics/commands/Vibrator.java
@@ -1,24 +1,16 @@
 package org.stlpriory.robotics.commands;
 
 import org.stlpriory.robotics.Robot;
-import org.stlpriory.robotics.utils.Pope;
-import org.stlpriory.robotics.utils.Pope.religions;
 
 import edu.wpi.first.wpilibj.Joystick.RumbleType;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Vibrator extends Command {
-	private boolean isPopeCatholic;
-	private Pope pope;
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
 		Robot.oi.getController().setRumble(RumbleType.kLeftRumble, 1);
 		Robot.oi.getController().setRumble(RumbleType.kRightRumble, 1);
-		this.pope = new Pope(religions.Catholic);
-		isPopeCatholic = isPopeCatholic(pope);
-		
-
 	}
 
 	@Override
@@ -27,13 +19,12 @@ public class Vibrator extends Command {
 		Robot.oi.getController().setRumble(RumbleType.kRightRumble, (float) Math.random());
 		Robot.oi.getController().setRumble(RumbleType.kLeftRumble, (float) Math.random());
 		System.out.println("THAT'S THE WRONG BUTTON");
-		isPopeCatholic = isPopeCatholic(pope);
 	}
 
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-		return isPopeCatholic;
+		return true;
 	}
 
 	@Override
@@ -47,9 +38,4 @@ public class Vibrator extends Command {
 		// TODO Auto-generated method stub
 
 	}
-	private boolean isPopeCatholic(Pope p)
-	{
-		return p.religion == religions.Catholic;
-	}
-
 }
